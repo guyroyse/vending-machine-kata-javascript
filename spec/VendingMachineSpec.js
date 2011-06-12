136eb853d3aa75777f66ec3418dbc1a21a2c94d4
Guy Royse <guy@guyroyse.com>
2011-06-11 23:18:03 -0400

    Initial commit

---
+++ spec/VendingMachineSpec.js
@@ -0,0 +1,21 @@
+describe("Jasmine Test Runner", function() {
+
+  it("Runs", function() {
+    expect(true).toBeTruthy();
+    expect(false).toBeFalsy();
+    expect(42).toEqual(42);
+  });
+
+  describe("Vending Machine", function() {
+
+    beforeEach(function() {
+      // any startup code goes here
+    });
+
+    it("..first test goes here..", function() {
+    });
+    
+  });
+
+});
+
